5cc850b15fde874e0859be176a9f156b120c695c
Greg M. Crist, Jr <gmcrist@gmail.com>
2013-12-02 16:16:41 -0800

    Moved initial skeleton code to skeleton.py; added RFC 2821 documentation; added initial framework for the SMTP client

--- smtp_client.py
+++ smtp_client.py
@@ -1,49 +1,162 @@
-from socket import * 
-msg = ”\r\n I love computer networks!” 
-endmsg = ”\r\n.\r\n” 
-# Choose a mail server (e.g. Google mail server) and call it mailserver 
-mailserver = #Fill in start #Fill in end 
-# Create socket called clientSocket and establish a TCP connection with mailserver 
-#Fill in start 
- 
-#Fill in end 
-recv = clientSocket.recv(1024) 
-print recv 
-if recv[:3] != '220': 
-print '220 reply not received from server.' 
- 
-# Send HELO command and print server response. 
-heloCommand = 'HELO Alice\r\n' 
-clientSocket.send(heloCommand) 
-recv1 = clientSocket.recv(1024) 
-print recv1 
-if recv1[:3] != '250': 
- print '250 reply not received from server.' 
- 
-# Send MAIL FROM command and print server response. 
-# Fill in start 
- 
-# Fill in end 
- 
-# Send RCPT TO command and print server response. 
-# Fill in start 
- 
-# Fill in end 
- 
-# Send DATA command and print server response. 
-# Fill in start 
- 
-# Fill in end 
- 
-# Send message data. 
-# Fill in start 
- 
-# Fill in end # Message ends with a single period.
-# Fill in start
- 
-# Fill in end 
- 
-# Send QUIT command and get server response. 
-# Fill in start 
- 
-# Fill in end
+##
+# Programming Assignment 2
+# CPE 138, Fall 2013
+# California State University, Sacramento
+#
+# This is SMTP client
+#
+# Created by Greg M. Crist, Jr. <gmcrist@gmail.com> & Travis Spitze <travissp87@gmail.com>
+##
+
+import logging      # For logging / debug messages
+import socket       # Network socket coding
+import ssl          # SSL code
+import time         # Date / time functions
+
+
+# Default configuration for the server
+config = {
+    'host': 'smtp.fireup.net',
+    'port': '25',
+    'ssl': True,
+    'loglevel': logging.DEBUG
+}
+
+class SmtpClient ():
+    _defaultConfig = {
+        'host': 'localhost',
+        'port': 25,
+        'ssl': False,
+        'timeout': socket._GLOBAL_DEFAULT_TIMEOUT,
+    }
+
+    def __init__(self, config):
+        self.config = dict(self._defaultConfig.items() + config.items())
+        self._logger = logging.getLogger('SmtpClient')
+        self.file = None
+        
+        # RFC 2821 requires us to use the fully qualified domain name for the EHLO/HELO commands
+        fqdn = socket.getfqdn()
+
+        if '.' in fqdn:
+            # We have a valid fqdn
+            self.config['localhostname'] = fqdn
+        else:
+            # If we can't determine the fqdn, we should use an encoded IP address
+            addr = '127.0.0.1'
+            
+            try:
+                addr = socket.gethostbyname(socket.gethostname())
+            except socket.gaierror:
+                pass
+
+            self.config['localhostname'] = '[' + addr + ']'
+
+    ##
+    # Connect socket
+    ##
+    def connect(self):
+        self._logger.info('Creating connection to SMTP server ' + self.config['host'] + ' on port ' + str(self.config['port']))
+        self.socket = socket.create_connection((self.config['host'], self.config['port']), self.config['timeout'])
+
+    ##
+    # Disconnect socket
+    ##
+    def disconnect(self):
+        self._logger.info('Closing connection to SMTP server')
+        
+        if (self.file):
+            self.file.close()
+            self.file = None
+            
+        if (self.socket):
+            self.socket.close()
+            self.socket = None
+
+    ##
+    # Send data to socket
+    ##
+    def send(self, data):
+        self._logger.debug('Sending data to SMTP server')
+        if (self.socket):
+            try:
+                self.socket.sendall(data)
+            except socket.error:
+                self._logger.error('Error sending data to socket')
+                self.disconnect();
+                raise Exception('error sending data to socket')
+        else:
+            self._logger.error('no socket connection')
+            raise Exception('no socket connection')
+
+    ##
+    # Execute SMTP command and get response
+    ##
+    def command(self, command, data):
+        self._logger.info('Sending command "' + command + '" to SMTP server')
+        if (data == ''):
+            data = command + "\r\n"
+        else:
+            data = command + ' ' + data + "\r\n"
+        
+        self.send(data)
+        return self.response()
+
+    ##
+    # Get repsponse from SMTP server
+    ##
+    def response(self):
+        response = []
+        
+        # It is easier to use the socket readline method
+        if (self.file is None):
+            self.file = self.socket.makefile('rb')
+        
+        while True:
+            try:
+                line = self.file.readline()
+                
+            except socket.error:
+                self._logger.error('Connection to SMTP server closed unexpectedly');
+                self.disconnect();
+                raise Exception('Connection to SMTP server closed unexpectedly');
+            
+            self._logger.debug(line);
+            
+            
+            # The response code is always a 3-digit number
+            code = int(line[:3])
+            
+            # Append the response data (no need to store the message)
+            response.append(line[4:].strip())
+
+            # Multi-line responses have a "-" character in between the code and response
+            if (line[3:4] == '-'):
+                break
+    
+        return code, response            
+
+    def helo(self):
+        [code, response] = self.command('HELO', self.config['localhostname']);
+        self._logger.info('Received ' + code + ' response with response: ' + response)
+
+    def ehlo(self):
+        [code, response] = self.command('EHLO', self.config['localhostname']);
+        
+    
+
+
+    def sendmail(self, fromAddr, toAddrs, subject, message):
+        self.setFromAddr(fromAddr)
+        self.addRecipients(toAddrs)
+        self.setSubject(subject)
+        self.setMessage(message)
+        self.send()
+        
+        
+logging.basicConfig(format='%(levelname)s:%(message)s', level=config['loglevel'])
+client = SmtpClient(config);
+
+client.connect();
+client.helo();
+
